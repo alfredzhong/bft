8880e55f19ea1fa17e244103f049cd79f7ce0919
Alfred Zhong <az@cs.brown.edu>
2013-12-12 21:09:16 -0500

    add concurrent size()

--- bft_bft.h
+++ bft_bft.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
+#include <immintrin.h>
 
 #include <bft_layer.h>
 
@@ -20,12 +21,14 @@ namespace bft {
         private:
             std::vector<bft::bft_layer<K, V> > *layers;
             int cur_size; 
+            std::mutex *fallback_mutex;
         public:
             bft_bft(int first_layer_size, int initial_layers) {
                 layers = new std::vector<bft::bft_layer<K, V> >();
                 layers->reserve(initial_layers);
                 bft::bft_layer<K, V> first_layer(first_layer_size);
                 layers->push_back(first_layer);
+                fallback_mutex = new std::mutex();
             }
 
             bft_bft() {
@@ -33,12 +36,32 @@ namespace bft {
                 layers->reserve(DEFAULT_INIT_LAYERS);
                 bft::bft_layer<K, V> first_layer;
                 layers->push_back(first_layer);
+                fallback_mutex = new std::mutex();
             }
 
             ~bft_bft() {
                 cur_size = 0;
                 delete layers;
             }
+
+            // thread safe size()
+            int size() {
+                int ret = 0;
+                if (_xbegin() == -1) {
+                    ret = cur_size;
+                    _xend();
+                } else {
+                    fallback_mutex->lock();
+                    ret = cur_size;
+                    fallback_mutex->unlock();
+                }
+                return ret;
+            }
+
+            int add(bft::bft_node<K, V> node) {
+                return 0;
+            }
+
     };
 }
 
